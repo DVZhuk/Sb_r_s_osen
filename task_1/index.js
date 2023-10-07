b823743d24feb7027685cff997071ea26767cd71
Denis <XamlBit@yandex.ru>
2023-10-07 18:46:56 +0300

    modified:   task_1/index.js

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,34 @@
 import {encoded, translations} from './data.js'
 
 console.log("Let's rock")
-console.log(encoded, translations)
+
+// console.log(encoded, translations)
 
 
+const decoded = function (encoded, translations) {
+    return encoded.map(obj => {
+        for (let key in obj) {
+            if (translations.hasOwnProperty(obj[key])) {
+                obj[key] = translations[obj[key]];
+            } else {
+                obj[key] = obj[key];
+            };
+        };
+        return obj
+    });
+};
 
-// console.log(decoded)
+const unique = function (encoded, translations) {
+    const uniqueNumArr = [];
+    encoded.forEach(obj => {
+        for (let key in obj) {
+            if (!translations.hasOwnProperty(obj[key]) && !Number.isNaN(Number.parseInt(obj[key])) && key !== 'groupId') {
+                uniqueNumArr.push(obj[key])
+            };
+        };
+    });
+    return uniqueNumArr
+}
+
+console.log(decoded(encoded, translations))
+console.log(unique(encoded, translations))
